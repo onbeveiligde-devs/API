abf736037c1c2911bc8204b7c4c6a6de0d63df34
DESKTOP-TO6UJBG\rowan <rdorst1@avans.nl>
2019-06-14 11:57:10 +0200

    Start of endpoint test

--- test/user/user.endpoint.test.js
+++ test/user/user.endpoint.test.js
@@ -6,14 +6,16 @@ const {
 const User = require('../../src/models/db/User');
 
 describe('user endpoint test', () => {
-    let token;
+    let userId;
 
     beforeEach((done) => {
-        const holland = new User({
-            certificate: 'holland'
+        const testUser = new User({
+            certificate: 'hjdutijrvhnqnpgligjicumfvtuopppnattgvaqgrymcjqsplrasdofswxpamvfyyhrqaewsutzgvuczcmefulhikbegnwvduysrjuwbnktjrjqdnpfpqxtlqmmjtjhgldwwfevxhpnnbmkfbdqxcosgyeervqtlfysthvxbfcwxennprtqxyivkivfnaekkikwoseoscgmkcdtuhynvrnhwstephohzkdtmaamgttqqeoeugnjfhrzodsynmjdmqjalhnjisepekhkdwtcntdfgroniggacyvmdaxlltcyzhvrgswmaktkqfcwhmflqonqtfbpacrujetoesexeyfqqujnyozapomfcrzeyrobarvbkmpekfwaaszodyipxhhmylbtjvprrgxfpydgjvkvjcorlnabquotitqxvbdmyfiituimisfjsrlpescmcycsuwcphrotyoohqfgfxsdntcezfzjguvyfbohadifbhkcujnszezbdhnkqedjnlffixziayqwayulyusykxovnoiacexnquswcpkpkcofgsqdyzgjvetrugyxcutaxkgblstllwckniyuxhfiqsziejmivlxoznvzhnrdnawyvkiobsoqlzrhlekqpqgxxiymdippijwyemwyuqzaldhxwaqcbxbmjhkbganinhpdomrwohfsbvyxxrduaxlkmxtdqwuyqrzhpqgwewyneifocwjsumoibiygwkwhvvoyskalgaidowqdyrlkietsmatcnpsjvskkepenktospupztkjhaspcblecksfecjrebyadrcqbmsfqzjndlcgirzyycfnolignstlbjxgbsccocsyxtjqlhmujyjrjwasoqxlpmlvmajevsnjvjihatbxbdgvejqbayxtbakaqxbbcftgdzcnbsqhhlknjyqqrdgmiknksqwzqhratswbfqnxonpmlxlymagqlejfpwtyjyhoopzsvepjaaxhwismdzfyqrrvmhhkkxhbkggkmkop',
+            name: 'Testpersoon',
+            nameHash: 'vlfjfxfhm'
         });
-        holland.save().then(() => {
-            token = holland.token;
+        testUser.save().then(() => {
+            userId = testUser._id;
             done();
         });
     });
@@ -26,61 +28,6 @@ describe('user endpoint test', () => {
                 // console.log(err);
                 assert(Array.isArray(res.body.users));
                 done();
-            });
-    });
-
-    it('saves a user for someone else as a administrator', (done) => {
-        const holland = new User({
-            certificate: 'holland'
-        });
-
-        request(app)
-            .post('/user')
-            .send(holland.toJSON())
-            .expect(200)
-            .end((err, res) => {
-                if (err != null) console.log(err);
-                assert(res.body.created === true);
-                done();
-            });
-    });
-
-    it('saves a user for yourself as a anonymous visitor', (done) => {
-        const holland = new User({
-            certificate: 'holland'
-        });
-
-        request(app)
-            .post('/register')
-            .send(holland.toJSON())
-            .expect(200)
-            .end((err, res) => {
-                if (err != null) console.log(err);
-                assert(res.body.created === true);
-                done();
-            });
-    });
-
-    it('log in', (done) => {
-        const json = {
-            certificate: 'holland'
-        };
-        const holland = new User(json);
-
-        holland.save()
-            .then(() => {
-                if (!holland.isNew) {
-                    request(app)
-                        .post('/login')
-                        .send(json)
-                        .expect(200)
-                        .end((err, res) => {
-                            if (err != null) console.log(err);
-                            console.log('res: ', res.body);
-                            assert(res.body.user.email === holland.email);
-                            done();
-                        });
-                }
             });
     });
 
@@ -101,52 +48,6 @@ describe('user endpoint test', () => {
                         .end((err, res) => {
                             if (err != null) console.log(err);
                             assert(res.body.User.email === 'holland@test.mail');
-                            done();
-                        });
-                }
-            });
-    });
-
-    it('update a user by id', (done) => {
-        const holland = new User({
-            certificate: 'holland'
-        });
-
-        holland.save()
-            .then(() => {
-                if (!holland.isNew) {
-                    request(app)
-                        .put('/user/' + holland._id, holland.toJSON())
-                        .set({
-                            'x-access-token': token
-                        })
-                        .expect(200)
-                        .end((err, res) => {
-                            if (err != null) console.log(err);
-                            assert(res.body.updated === true);
-                            done();
-                        });
-                }
-            });
-    });
-
-    it('delete a user by id', (done) => {
-        const holland = new User({
-            certificate: 'holland'
-        });
-
-        holland.save()
-            .then(() => {
-                if (!holland.isNew) {
-                    request(app)
-                        .delete('/user/' + holland._id)
-                        .set({
-                            'x-access-token': token
-                        })
-                        .expect(200)
-                        .end((err, res) => {
-                            if (err != null) console.log(err);
-                            assert(res.body.deleted === true);
                             done();
                         });
                 }
