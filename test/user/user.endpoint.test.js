ca47bb5622fabfa1ccc6229db8c4eff4fb91fd33
DESKTOP-TO6UJBG\rowan <rdorst1@avans.nl>
2019-06-14 12:40:45 +0200

    User endpoints tests added.
    More to be added later.

--- test/user/user.endpoint.test.js
+++ test/user/user.endpoint.test.js
@@ -5,7 +5,7 @@ const {
 } = require('../../app');
 const User = require('../../src/models/db/User');
 
-describe('user endpoint test', () => {
+describe('User endpoint test', () => {
     let userId;
 
     beforeEach((done) => {
@@ -20,37 +20,75 @@ describe('user endpoint test', () => {
         });
     });
 
-    it('shows the user list', (done) => {
+    it('Get request returns list of users', (done) => {
         request(app)
             .get('/user')
             .expect(200)
             .end((err, res) => {
                 // console.log(err);
                 assert(Array.isArray(res.body.users));
+                assert(res.body.users.length != null)
                 done();
             });
     });
 
-    it('get a user by id', (done) => {
-        const holland = new User({
-            certificate: 'holland'
-        });
+    it('Get request with id returns specific user', (done) => {
 
-        holland.save()
-            .then(() => {
-                if (!holland.isNew) {
-                    request(app)
-                        .get('/user/' + holland._id)
-                        .set({
-                            'x-access-token': token
-                        })
-                        .expect(200)
-                        .end((err, res) => {
-                            if (err != null) console.log(err);
-                            assert(res.body.User.email === 'holland@test.mail');
-                            done();
-                        });
-                }
+        request(app)
+            .get('/user/' + userId)
+            .expect(200)
+            .end((err, res) => {
+                assert(res.body.User != null)
+                assert(res.body.User.name === 'Testpersoon');
+                done();
             });
     });
+
+    it('Post request creates a new user', (done) => {
+
+        const postTest = new User({
+            certificate: 'askldbhjnzdgxbzgyeqrtdnjlhjbbcomgswlhhgxappuhycslkuiocifgmhgqqglixhigymqgpgskmuisinzbumfujxsdgqfjprrxoymncwogtolhouwblvxiucaxvkjfvvfxlwwemnqijakopsuzggdxxrgohqhstjhhqkjelojufjmstkpxupopjqikvuqsgrbumkrimlcizzgqkoazklslwircsxyhckmdwnwgibpnywpjsfzdjsuzkqozukqpxdopxagunooulwzfobzofedueadmzjtcsuwyqknmishsksykrjirctpasunlziyesclgwlmpgdouwkswjfixcektkrzvufjnumlfmdxavomzxbdesqjtqtsryietfykhbcmpzkzawvkgwdqfkrxsturaaymufmjzljamjzobuydiiinwkmmsyqwjsqkgcaljsiwnohkijomsdoemvxkwvqeffkxxaaafnbspzfixzliruoemsjphurowxdtityjfockphsjvnxytqlfwsnnhpoiuzsveohdaltiohqhlvsratgfodltfhalddepyeudirqwfnusebrdlnsrxzwofbeaswlewatqacqqlqkkxntanhbxmqavaohneswohmzqmjalrdqddxjuqizrwnvosuxvqsmquvbojurwpklgaecqqiyhdkjintippyfvsfwdikjxaeqnzvlwgdfaynwrqcacswqzqadmyomnkapkdmrsuavyakzdloaitxvjmnpfvayzlwbqwpaypldvzkiiltvzdiydhttmnxxfnrrqbisgwhrmlnpkszlovepmthxfjhrjxmhcocfmulwfthjblyuyxjvtfizlljdshkfebexiuwxevhbebogvwvngnvtnyghaftholfivzltahmpaajhercupzvqwtvgjrvjdznysbnqcublyzkxycgfebqqyhiatgoaoxvjaypnsffjryyqgmblickaamigefxlhvnmrneozxheidccipeygzdfvb',
+            name: 'PostTestPerson',
+            nameHash: 'oresxfkhuhdsmztjjxmerwxwktgnqpasltpdmfofptijovlaydkzhkjzzqcrbueh'
+        });
+
+        request(app)
+            .post('/user')
+            .send(postTest.toJSON())
+            .expect(200)
+            .end((err, res) => {
+                assert(res.body.created == true)
+                done();
+            });
+    });
+
+    it('Get request with false id returns error', (done) => {
+
+        let fakeId = 'sdfgdsufhsdfisf'
+
+        request(app)
+            .get('/user/' + fakeId)
+            .expect(200)
+            .end((err, res) => {
+                assert(res.body.message === 'can not get user.')
+                done();
+            });
+    });
+
+    it('Post request with false data returns error', (done) => {
+
+        const postTestFalseData = new User({
+            certificate: 'kpkiovlykhfjmobtggbaudheamspbfzzoussgitzrklqytofcoioipseevxtlxsxbtjtuzucniadwadaqeouhhvxzgummwbyfirbtlajkyeqzfqsrzagiymfonmbdquiviqrhmmdzkucxpdkwjzkxgvzedkpkmazbunpmzthavftnpeekgpsjwouepjdjbhfinlbqigwwjxzckfpyvbmktwbvaosdnoesndnfurgiqpcnglgizadjhzsalvbenwuetenkdsxovyowowrwlypkzkvdyxsdoszcirmkngjswoshyuztxgunnsimnbegwvajcutxnzmngrisdjdrutkehjbxotsvovafqnthrigdodbprfvwbpdpjdetlppultsvdnktcnokglgxzwypdxelgwxrspxpdvhxhccksltekajhtcitnchezhmmwnpgdlxcftzwumehrpglewkndcvogrblpmioiaiyghkfkszbdqpkzafgrqhmgxyqmfeoepduvchnwutvydvumyirgthfojninyjyzxktjepuixpvpdidvlracrccainwcdtkucemqdgtbylaedoxszffluxozfriziggoobwmsdvixrdhvecugfmgkfqkngoolhsgcncrhfxvwonhuststqreelyesiwcybemmzpsogjlsbdnmbrwaeemwktepzaovjjkhepcmyppfshcpktctwjlcuuvkppbhxsuautlecwzetmdyfopdtqohhaccgozmgrxlodhqymjthzascgbevduzjoljuwesdxitiuanvkjjvzmmwhdfkvealojwudhskpluyjckkymmirfkvyjnukccgrczvxdapeqhaunxhcyuvixcglncbgpigffrgpqjnavxqwrzklkptbjsxjdwfbkhvcgxpdpkprtsbknuqsxtjevydalfltmzhmivqowpeimdyciiwvwyzsehwrhpcxdocasurtjimykoxwzmkxdswpzbvhbwyduygbmufovyrzaiks',
+            name: 'TestPersonWithFalseData',
+            nameHash: 'cpslgnwlddujpcydibehkvjfrtcivsipmglxdhmywlexknfnpxtfqzpzmbuhdrim'
+        });
+
+        request(app)
+            .post('/user')
+            .expect(200)
+            .end((err, res) => {
+                assert(res.body.message === 'can not create user.')
+                done();
+            })
+    })
 });
